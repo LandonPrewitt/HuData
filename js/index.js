0c51cacbe2fa062ce2d8e3794578b8b361175d2f
Peter French <peterfrench@dhcp-10-203-92-147.tamulink.tamu.edu>
2016-10-16 13:29:25 -0500

    Added more images

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-var images = ['stop-sign.jpg', 'granny.jpg', 'road.jpg'];
-var descriptions = ['a stop-sign', 'a person', 'a car'];
+var images = ['1_stop_sign.jpg', '2_granny.jpg','3_green_light.jpg', '4_Yield.png', '5_road.jpg', '6_people-crossing.jpg', '7_green_light.jpg', '8_highway.jpg'];
+var descriptions = ['Is there a stop-sign in this picture?', 'Is there a personin the way?', 'Is the light green?', 'Does this picture have a yeild sign in it?', 'Is there an obsruction in the road?', 'Is the road clear to proceed?', 'Is the light red?', 'Is there an obstruction in the way?'];
 i = 0;
 
 var nextImage = function() {
@@ -12,15 +12,41 @@ var nextImage = function() {
 };
 
 var nextDescription = function() {
-  $('#description').text('Is there ' + descriptions[i] + ' in the way?');
+  $('#description').text(descriptions[i]);
 };
 
 $('#yes').on('click', function() {
-  nextImage();
-  nextDescription();
+  $.ajax({
+      type: 'POST',
+      url: 'http://bhallasaurabh.com/add',
+      data: {
+        image: i,
+        result: 1
+      },
+      crossDomain: true,
+      dataType: 'json',
+      success: function(data) {
+        console.log(data);
+        nextImage();
+        nextDescription();
+      }
+  });
 });
 
 $('#no').on('click', function() {
-  nextImage();
-  nextDescription();
+  $.ajax({
+      type: 'POST',
+      url: 'http://bhallasaurabh.com/add',
+      data: {
+        image: i,
+        result: 1
+      },
+      crossDomain: true,
+      dataType: 'jsonp',
+      success: function(data) {
+        console.log(data);
+        nextImage();
+        nextDescription();
+      }
+  });
 });
