53e3f56748a3f7ae912a2ed7d7603be5a3850db0
Peter French <peterfrench@dhcp-10-203-92-147.tamulink.tamu.edu>
2016-10-16 13:39:29 -0500

    Fixed additional bugs

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 var images = ['1_stop_sign.jpg', '2_granny.jpg','3_green_light.jpg', '4_Yield.png', '5_road.jpg', '6_people-crossing.jpg', '7_green_light.jpg', '8_highway.jpg'];
-var descriptions = ['Is there a stop-sign in this picture?', 'Is there a personin the way?', 'Is the light green?', 'Does this picture have a yeild sign in it?', 'Is there an obsruction in the road?', 'Is the road clear to proceed?', 'Is the light red?', 'Is there an obstruction in the way?'];
+var descriptions = ['Is there a stop-sign in this picture?', 'Is there a person in the way?', 'Is the light green?', 'Does this picture have a yield sign in it?', 'Is there an obstruction in the road?', 'Is the road clear to proceed?', 'Is the light green?', 'Is the road clear to proceed?'];
 i = 0;
 
 var nextImage = function() {
