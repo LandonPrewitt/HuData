86eb80df47e63325cadac5e08b81e698743c5119
Peter French <peterfrench@dhcp-10-203-93-117.tamulink.tamu.edu>
2016-10-16 01:25:28 -0500

    Frontend base functionality, needs posting, more images, and graphs

---
+++ js/index.js
@@ -0,0 +1,26 @@
+var images = ['stop-sign.jpg', 'granny.jpg', 'road.jpg'];
+var descriptions = ['a stop-sign', 'a person', 'a car'];
+i = 0;
+
+var nextImage = function() {
+  i++;
+  if(i === images.length) {
+    window.location.replace('/graphs.html');
+  } else {
+    $('#image').attr('src', 'img/' + images[i]);
+  }
+};
+
+var nextDescription = function() {
+  $('#description').text('Is there ' + descriptions[i] + ' in the way?');
+};
+
+$('#yes').on('click', function() {
+  nextImage();
+  nextDescription();
+});
+
+$('#no').on('click', function() {
+  nextImage();
+  nextDescription();
+});
